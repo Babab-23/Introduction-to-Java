93c65b2501a12a98da07068c51b0e7b505743471
Babab-23 <adrianidialu@gmail.com>
2025-11-02 20:46:22 +0100

    first commit

---
+++ app.java
@@ -0,0 +1,8 @@
+public class app {
+    public static void main(String[] args) {
+         System.out.println("Hey Java");
+         System.out.println("Hey Adrian");
+         System.out.println("Hey Abdul");
+         System.out.println("Hey  Mr Elijah");
+    }
+}
